41ded4540dcdd59920b8c60d2a2172b98baba265
Fraser Balch <farseer.ulwithy@gmail.com>
2020-03-07 14:07:13 -0700

    packed vec is a waste

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,130 +1,198 @@
 use criterion::*;
+use oorandom::Rand64;
 use simd_and_parallel::*;
 
-const N: usize = 5_000;
+const N: usize = 50_000;
 
-fn scalar_magnitude_squared(c: &mut Criterion) {
-    let x = (0..).into_iter()
-        .map(|i| i as f64 * 0.9)
+fn get_floats_vec() -> Vec<(Float, Float)> {
+    (0..).into_iter()
+        .map(|i| (i as Float * 0.9, i as Float * 1.1))
         .take(N)
-        .collect::<Vec<_>>();
-
-    let y = (0..).into_iter()
-        .map(|i| i as f64 * 1.1)
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
-
-    c.bench_function("scalar" , |b| b.iter(|| magnitude_squared_fallback(&x, &y, &mut m)));
+        .collect()
 }
 
-fn parallel_scalar_magnitude_squared(c: &mut Criterion) {
-    let x = (0..).into_iter()
-        .map(|i| i as f64 * 0.9)
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let y = (0..).into_iter()
-        .map(|i| i as f64 * 1.1)
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
-
-    c.bench_function("par scalar" , |b| b.iter(|| parallel_magnitude_squared_fallback(&x, &y, &mut m)));
-}
-
-fn vector_magnitude_squared(c: &mut Criterion) {
-    let x = (0..).into_iter()
-        .map(|i| i as f64 * 0.9)
-        .take(N)
-        .fold(Vec::with_capacity(N), |mut v, i| {
-            v.push(i);
+fn vectors_add_assign(c: &mut Criterion) {
+    let mut lhs = get_floats_vec()
+        .into_iter()
+        .map(|(x, y)| Vector { x, y })
+        .fold(Vectors::default(), |mut v, val| {
+            v.val.push(val);
             v
         });
 
-    let y = (0..).into_iter()
-        .map(|i| i as f64 * 1.1)
-        .take(N)
-        .fold(Vec::with_capacity(N), |mut v, i| {
-            v.push(i);
+    let rhs = lhs.clone();
+
+    c.bench_function(
+        "vectors add assign",
+        |b| b.iter(|| {
+            lhs += &rhs;
+        })
+    );
+}
+
+fn vec2_add_assign(c: &mut Criterion) {
+    let mut lhs = get_floats_vec()
+        .into_iter()
+        .fold(Vec2::default(), |mut v, (x, y)| {
+            v.x.push(x);
+            v.y.push(y);
             v
         });
 
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
+    let rhs = lhs.clone();
 
-    c.bench_function("vector" , |b| b.iter(|| magnitude_squared(&x, &y, &mut m)));
-}
-
-fn parallel_vector_magnitude_squared(c: &mut Criterion) {
-    let x = (0..).into_iter()
-        .map(|i| i as f64 * 0.9)
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let y = (0..).into_iter()
-        .map(|i| i as f64 * 1.1)
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
-
-    c.bench_function("par vector" , |b| b.iter(|| parallel_magnitude_squared(&x, &y, &mut m)));
-}
-
-fn packed_vector(c: &mut Criterion) {
-    let v = (0..).into_iter()
-        .map(|i| f64x2::new(i as f64 * 0.9, i as f64 * 1.1))
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
-
-    c.bench_function("packed vector" , |b| b.iter(|| packed_magnitude_squared(&v, &mut m)));
-}
-
-fn vector_type(c: &mut Criterion) {
-    let v = (0..).into_iter()
-        .map(|i| {
-            let x = i as f64 * 0.9;
-            let y = i as f64 * 1.1;
-            Vector { x, y }
+    c.bench_function(
+        "vec2 add assign",
+        |b| b.iter(|| {
+            lhs += &rhs;
         })
-        .take(N)
-        .collect::<Vec<_>>();
-
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
-
-    c.bench_function("vector type", |b| b.iter(|| Vector::msqrd(&v, &mut m)));
+    );
 }
 
-fn par_vector_type(c: &mut Criterion) {
-    let v = (0..).into_iter()
-        .map(|i| {
-            let x = i as f64 * 0.9;
-            let y = i as f64 * 1.1;
-            Vector { x, y }
+const ATTRACTORS: usize = 100;
+const BODIES: usize = 5000;
+
+fn get_vec2(rng: &mut Rand64, n: usize) -> Vec2 {
+    (0..n)
+        .into_iter()
+        .fold(Vec2::default(), |mut v, _| {
+            v.x.push(rng.rand_float() as Float);
+            v.y.push(rng.rand_float() as Float);
+            v
         })
-        .take(N)
-        .collect::<Vec<_>>();
+}
 
-    let mut m = (0..).into_iter().map(|_| 0f64).take(N).collect::<Vec<_>>();
+fn get_vec(rng: &mut Rand64, n: usize) -> Vec1 {
+    Vec1 {
+        val: (0..n)
+            .into_iter()
+            .map(|_| rng.rand_float() as Float)
+            .collect()
+    }
+}
 
-    c.bench_function("par vector type", |b| b.iter(|| Vector::par_msqrd(&v, &mut m)));
+fn get_link(rng: &mut Rand64, n: usize, range: usize) -> Vec<usize> {
+    (0..n)
+        .into_iter()
+        .map(|_| rng.rand_range(0..range as u64) as usize)
+        .collect()
+}
+
+fn get_empty_vec2(n: usize) -> Vec2 {
+    Vec2 {
+        x: (0..n).into_iter().map(|_| 0.0).collect(),
+        y: (0..n).into_iter().map(|_| 0.0).collect(),
+    }
+}
+
+fn get_empty_vec(n: usize) -> Vec1 {
+    Vec1 {
+        val: (0..n).into_iter().map(|_| 0.0).collect()
+    }
+}
+
+const G: Float = 6.674e-11;
+const DT: Float = 0.1;
+
+fn vec2_gravity(c: &mut Criterion) {
+    let mut rng = Rand64::new(0);
+
+    let att_mass = get_vec(&mut rng, ATTRACTORS);
+    let att_pos = get_vec2(&mut rng, ATTRACTORS);
+
+    let body_pos = get_vec2(&mut rng, BODIES);
+    let mut body_vel = get_vec2(&mut rng, BODIES);
+    let body_att = get_link(&mut rng,  BODIES, ATTRACTORS);
+    let mut parent_pos = get_empty_vec2(BODIES);
+    let mut parent_mass = get_empty_vec(BODIES);
+
+    c.bench_function(
+        "vec2 gravity",
+        |b| b.iter(|| {
+            // GET PARENT POSITION
+            parent_pos.get_from(&body_att, &att_pos);
+
+            // GET PARENT MASS
+            parent_mass.get_from(&body_att, &att_mass);
+
+            // CALCULATE FORCE DUE TO GRAVITY
+            parent_pos -= &body_pos;
+
+            body_vel.x.iter_mut()
+                .zip(body_vel.y.iter_mut())
+                .zip(parent_pos.x.iter())
+                .zip(parent_pos.y.iter())
+                .zip(parent_mass.val.iter())
+                .for_each(|((((bvx, bvy), px), py), pm)| {
+                    let dist_squared = (*px * *px) + (*py * *py);
+                    let dist = dist_squared.sqrt();
+                    let ux = *px / dist;
+                    let uy = *py / dist;
+                    let a = G * DT * *pm / dist_squared;
+                    *bvx += ux * a;
+                    *bvy += uy * a;
+                })
+        })
+    );
+}
+
+fn get_vector(rng: &mut Rand64, n: usize) -> Vectors {
+    Vectors {
+        val: (0..n)
+            .into_iter()
+            .map(|_| Vector { x: rng.rand_float() as Float, y: rng.rand_float() as Float })
+            .collect()
+    }
+}
+
+fn vector_gravity(c: &mut Criterion) {
+    let mut rng = Rand64::new(0);
+
+    let att_mass = get_vec(&mut rng, ATTRACTORS);
+    let att_pos = get_vector(&mut rng, ATTRACTORS);
+
+    let body_pos = get_vector(&mut rng, BODIES);
+    let mut body_vel = get_vector(&mut rng, BODIES);
+    let body_att = get_link(&mut rng,  BODIES, ATTRACTORS);
+
+    c.bench_function(
+        "vector gravity",
+        |b| b.iter(|| {
+            body_vel.val.iter_mut()
+                .zip(body_pos.val.iter())
+                .zip(body_att.iter())
+                // .zip(parent_pos.val.iter())
+                // .zip(parent_mass.iter())
+                .filter_map(|((v, p), a)| {
+                    let p_pos = *att_pos.val.get(*a)? - *p;
+                    let pm = *att_mass.val.get(*a)?;
+
+                    Some((v, p_pos, pm))
+                })
+                .for_each(|(bv, p_pos, pm)| {
+                    let dist_squared = p_pos.magnitude_squared();
+                    let dist = dist_squared.sqrt();
+                    let u = p_pos / dist;
+                    let a = G * DT * pm / dist_squared;
+                    *bv += u * a;
+                })
+        })
+    );
 }
 
 criterion_group!(
-    functions,
-    scalar_magnitude_squared,
-    vector_magnitude_squared,
-    parallel_scalar_magnitude_squared,
-    parallel_vector_magnitude_squared,
-    packed_vector,
-    vector_type,
-    par_vector_type
+    add_assign,
+    vectors_add_assign,
+    vec2_add_assign,
+);
+
+criterion_group!(
+    gravity,
+    vec2_gravity,
+    vector_gravity,
 );
 
 criterion_main! {
-    functions
+    add_assign,
+    gravity,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,250 @@
-use rayon::prelude::*;
-pub use packed_simd::f64x2;
+use std::ops::*;
 
-
-pub fn magnitude_squared(x: &[f64], y: &[f64], m: &mut [f64]) {
-   #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-   {
-       if is_x86_feature_detected!("avx2") {
-           return unsafe { magnitude_squared_avx2(x, y, m); }
-       }
-   }
-
-    return magnitude_squared_fallback(x, y, m);
-}
-
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[target_feature(enable = "avx2")]
-unsafe fn magnitude_squared_avx2(x: &[f64], y: &[f64], m: &mut [f64]) {
-    magnitude_squared_fallback(x, y ,m);
-}
-
-pub fn magnitude_squared_fallback(x: &[f64], y: &[f64], m: &mut [f64]) {
-    m.iter_mut()
-        .zip(x.iter())
-        .zip(y.iter())
-        .for_each(|((m, x),  y)| {
-            *m = *x * *x + *y * *y;
-        })
-}
-
-pub fn parallel_magnitude_squared_fallback(x: &[f64], y: &[f64], m: &mut [f64]) {
-    m.par_iter_mut()
-        .zip(x.par_iter())
-        .zip(y.par_iter())
-        .for_each(|((m, x), y)| {
-            *m = *x * *x + *y * *y;
-        })
-}
-
-pub fn parallel_magnitude_squared(x: &[f64], y: &[f64], m: &mut [f64]) {
-    m.par_chunks_mut(256)
-        .zip(x.par_chunks(256))
-        .zip(y.par_chunks(256))
-        .for_each(|((m, x), y)| {
-            magnitude_squared(x, y, m);
-        })
-}
-
-pub fn packed_magnitude_squared(v: &[f64x2], m: &mut [f64]) {
-    m.iter_mut()
-        .zip(v.iter())
-        .for_each(|(m, v)| {
-            *m = (*v * *v).sum();
-        })
-}
+pub type Float = f32;
 
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
 pub struct Vector {
-    pub x: f64,
-    pub y: f64,
+    pub x: Float,
+    pub y: Float,
 }
 
 impl Vector {
-    pub fn magnitude_squared(self) -> f64 {
+    pub fn magnitude_squared(self) -> Float {
         self.x * self.x + self.y * self.y
     }
 
-    pub fn msqrd(v: &[Vector], m: &mut [f64]) {
+    pub fn msqrd(v: &[Vector], m: &mut [Float]) {
         v.iter()
             .zip(m.iter_mut())
             .for_each(|(v, m)| {
                 *m = v.magnitude_squared();
             });
     }
+}
 
-    pub fn par_msqrd(v: &[Vector], m: &mut [f64]) {
-        v.par_iter()
-            .zip(m.par_iter_mut())
-            .for_each(|(v, m)| {
-                *m = v.magnitude_squared();
+impl AddAssign for Vector {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl SubAssign for Vector {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+impl Sub for Vector {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y
+        }
+    }
+}
+
+impl Mul<Float> for Vector {
+    type Output = Self;
+
+    fn mul(self, rhs: Float) -> Self::Output {
+        Vector {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+impl Div<Float> for Vector {
+    type Output = Self;
+
+    fn div(self, rhs: Float) -> Self::Output {
+        Vector {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Vectors {
+    pub val: Vec<Vector>,
+}
+
+impl AddAssign<&Self> for Vectors {
+    fn add_assign(&mut self, rhs: &Vectors) {
+        self.val.iter_mut()
+            .zip(rhs.val.iter())
+            .for_each(|(s, r)| {
+                *s += *r;
             })
     }
+}
+
+impl SubAssign<&Self> for Vectors {
+    fn sub_assign(&mut self, rhs: &Vectors) {
+        self.val.iter_mut()
+            .zip(rhs.val.iter())
+            .for_each(|(s, r)| {
+                *s -= *r;
+            })
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Vec1 {
+    pub val: Vec<Float>,
+}
+
+impl Vec1 {
+    pub fn get_from(&mut self, indices: &Vec<usize>, values: &Vec1) {
+        self.val.iter_mut()
+            .zip(indices.iter())
+            .for_each(|(v, i)| {
+                if let Some(val) = values.val.get(*i) {
+                    *v = *val;
+                }
+            });
+    }
+
+    pub fn get_magnitude(&mut self, vec: &Vec2) {
+        self.val.iter_mut()
+            .zip(vec.x.iter())
+            .zip(vec.y.iter())
+            .for_each(|((v, x), y)| {
+                *v = ((*x * *x) + (*y * *y)).sqrt();
+            })
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Vec2 {
+    pub x: Vec<Float>,
+    pub y: Vec<Float>,
+}
+
+impl Vec2 {
+    pub fn get_from(&mut self, indices: &Vec<usize>, values: &Vec2) {
+        self.x.iter_mut()
+            .zip(self.y.iter_mut())
+            .zip(indices.iter())
+            .for_each(|((x, y), i)| {
+                if let Some(val) = values.x.get(*i) {
+                    *x = *val;
+                }
+                if let Some(val) = values.y.get(*i) {
+                    *y = *val;
+                }
+            });
+    }
+}
+
+impl AddAssign<&Self> for Vec2 {
+    fn add_assign(&mut self, rhs: &Vec2) {
+        self.x.iter_mut()
+            .zip(rhs.x.iter())
+            .for_each(|(s, r)| {
+                *s += *r;
+            });
+
+        self.y.iter_mut()
+            .zip(rhs.y.iter())
+            .for_each(|(s, r)| {
+                *s += *r;
+            });
+    }
+}
+
+impl SubAssign<&Self> for Vec2 {
+    fn sub_assign(&mut self, rhs: &Vec2) {
+        self.x.iter_mut()
+            .zip(rhs.x.iter())
+            .for_each(|(s, r)| {
+                *s -= *r;
+            });
+
+        self.y.iter_mut()
+            .zip(rhs.y.iter())
+            .for_each(|(s, r)| {
+                *s -= *r;
+            });
+    }
+}
+
+impl MulAssign<&Vec1> for Vec2 {
+    fn mul_assign(&mut self, rhs: &Vec1) {
+        self.x.iter_mut()
+            .zip(rhs.val.iter())
+            .for_each(|(s, r)| {
+                *s *= *r;
+            });
+
+        self.y.iter_mut()
+            .zip(rhs.val.iter())
+            .for_each(|(s, r)| {
+                *s *= *r;
+            });
+    }
+}
+
+impl DivAssign<&Vec1> for Vec2 {
+    fn div_assign(&mut self, rhs: &Vec1) {
+        self.x.iter_mut()
+            .zip(rhs.val.iter())
+            .for_each(|(s, r)| {
+                *s /= *r;
+            });
+
+        self.y.iter_mut()
+            .zip(rhs.val.iter())
+            .for_each(|(s, r)| {
+                *s /= *r;
+            });
+    }
+}
+
+#[test]
+fn add_assign_test() {
+    let mut a = Vec2 {
+        x: vec![0.0, 1.0, 2.0],
+        y: vec![0.0, 2.0, 3.0],
+    };
+
+    let b = Vec2 {
+        x: vec![1.0, 1.0, 1.0],
+        y: vec![1.0, 1.0, 1.0],
+    };
+
+    let expected = Vec2 {
+        x: vec![1.0, 2.0, 3.0],
+        y: vec![1.0, 3.0, 4.0],
+    };
+
+    a += &b;
+
+    assert_eq!(expected, a);
+}
+
+#[test]
+fn mul_assign_test() {
+    let mut a = Vec2 {
+        x: vec![0.0, 1.0, 2.0],
+        y: vec![0.0, 2.0, 3.0],
+    };
+
+    let b = Vec1 {
+        val: vec![2.0, 3.0, 5.0],
+    };
+
+    let expected = Vec2 {
+        x: vec![0.0, 3.0, 10.0],
+        y: vec![0.0, 6.0, 15.0],
+    };
+
+    a *= &b;
+
+    assert_eq!(expected, a);
 }
